e4e132392eb38432ebc2e26cf7245aca800fe957
jobayer34 <jobayercse@gmail.com>
2016-10-20 00:18:09 +0600

    column filter & state save option added.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<table id="example" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" >
+<table id="example" class="table table-striped table-bordered dt-responsive " cellspacing="0" >
     <thead>
         <tr>
             <th>Order Number</th>
@@ -8,16 +8,7 @@
             <th>Order LineNumber</th>
         </tr>
     </thead>
- 
-    <tfoot>
-        <tr>
-           <th>Customer Number</th>
-            <th>Name</th>
-            <th>Phone</th>
-            <th>Address</th>
-            <th>City</th>
-        </tr>
-    </tfoot>
+
 </table>
 
 <!-- Latest compiled and minified CSS -->
@@ -26,12 +17,16 @@
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" >
 
 <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.1.0/css/responsive.bootstrap.min.css" >
+<link href="jquery.dataTables.yadcf.css" rel="stylesheet" />
+
 
 <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.3.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/responsive/2.1.0/js/dataTables.responsive.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/responsive/2.1.0/js/responsive.bootstrap.min.js"></script>
+
+<script type="text/javascript" src="jquery.dataTables.yadcf.js"></script>
 
 <script>
 var columns_short = [   
@@ -43,7 +38,16 @@ var columns_short = [
                     ];
 
 $(document).ready(function() {
-    $('#example').dataTable( {
+    var tableElement = $('#example');
+    var exampleTable = tableElement.DataTable( {
+        //state save for remembering last visited page using localstorage, we have used localstorage, because if list is very big, it can't store all data to cookies
+        "stateSave": true,
+        "stateSaveCallback": function (settings, data) {
+            localStorage.setItem('DataTables_example_state', JSON.stringify(data))
+        },
+        "stateLoadCallback": function (settings) {
+            return JSON.parse(localStorage.getItem('DataTables_example_state'))
+        },
         "processing": true,
         "serverSide": true,
         // If you need to change any columns appearance or need to add any custom html, you can do it using columnDefs
@@ -74,5 +78,24 @@ $(document).ready(function() {
             "data": {"table": "orderdetails", "primary_key": "id", "page": "order_details", "columns": columns_short}
         }
     });
+
+    //adding tfoot using js, after loading main table, otherwise yadcf will not work properly
+    $("#example").append('<tfoot><tr><th></th><th></th><th></th><th></th><th></th></tr></tfoot>');
+
+    //individual column filtering using yadcf plugin
+    yadcf.init(exampleTable, [
+        {column_number: 1, filter_type: "text"},
+        {column_number: 2, filter_type: "text"},
+      ], 'footer');
 });
+
+
 </script>
+
+<style>
+    @media (max-width: 1023px) {
+      #example tfoot {
+        display: none;
+      }
+    }
+</style>
